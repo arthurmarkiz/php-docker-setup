f9eafd08b41bfc30b58c894addb00bb9e7cac752
Arthur Markiz <tutumarkiz26@gmail.com>
2024-09-10 12:40:38 -0300

    First commit

---
+++ php-tailwind/src/public/includes/footer.php
@@ -0,0 +1,3 @@
+<footer class="bg-gray-700 text-white text-center p-4 mt-10">
+    2024 &copy; All rights reserved.
+</footer>
---
+++ php-tailwind/src/public/includes/header.php
@@ -0,0 +1,10 @@
+<header class="bg-blue-500 text-white p-4">
+    <h1>My Website</h1>
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="contact.php">Contact</a></li>
+        </ul>
+    </nav>
+</header>
